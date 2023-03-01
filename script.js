d0fd84680a61b3e46ecbc8723bfb0a543eb5b608
mrwoodsisharmless <125097120+mrwoodsisharmless@users.noreply.github.com>
2023-02-28 20:35:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
+//this script no longer works
 CUSTOM CURSOR (44x44 IMAGE) (TO RESIZE YOUR IMAGE GO TO THIS SITE:https://www.resizepixel.com): javascript:(function()%7B var style %3D document.createElement(%27style%27), styleContent %3D document.createTextNode(%27* %7B cursor: url(YOUR IMAGE LINK HERE!) 22 22, auto !important%7D%27)%3B style.appendChild(styleContent )%3B var caput %3D document.getElementsByTagName(%27head%27)%3B caput%5B0%5D.appendChild(style)%3B %7D)()%3B
 
